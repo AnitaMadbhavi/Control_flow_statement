9389bdc764fa144146eeab8de091168a7ebef68c
AnitaMadbhavi <anitamadbhavi82xworkz@gmail.com>
2021-12-22 15:57:36 +0530

    Added java file to print Monday

---
+++ Monday.java
@@ -0,0 +1,16 @@
+class Monday{
+	public static void main(String []args){
+	//System.out.println("Monday");
+
+	/*int number;
+	for(number = 1; number < 11; number = number + 1){
+	System.out.println(number);
+	}*/
+	int num;
+	for(num=1; num<50; num=num+1)
+	{
+	System.out.println(num);
+	num=num+num;
+	}
+	}
+}
