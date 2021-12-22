ffa39c561bedf4a89f21fed36ba13b84ce300c2b
AnitaMadbhavi <anitamadbhavi82xworkz@gmail.com>
2021-12-22 16:01:54 +0530

    initialization removed

---
+++ Num1.java
@@ -0,0 +1,10 @@
+class Num1{
+	//error: initilization must b there
+	public static void main(String []args){
+	//int num =0;
+	int num;
+	for(; num<10; num=num+1){
+	System.out.println(num);
+	}
+	}
+}
