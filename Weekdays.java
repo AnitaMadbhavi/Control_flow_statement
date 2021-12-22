c6835f5d0cf2ae699f4e8294dcdabd8311217603
AnitaMadbhavi <anitamadbhavi82xworkz@gmail.com>
2021-12-22 16:08:52 +0530

    displays weeks

---
+++ Weekdays.java
@@ -0,0 +1,6 @@
+class Weekdays{
+	public static void main(String []annu){
+	String []days={"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+	System.out.println(days[1]);
+}
+}
